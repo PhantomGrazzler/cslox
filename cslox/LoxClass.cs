3a63bb470611755b7daad2ba149280046efa2661
PhantomGrazzler <PhantomGrazzler@users.noreply.github.com>
2025-04-18 18:02:19 +1000

    Completed section 12.3 (Creating Instances).

--- cslox/LoxClass.cs
+++ cslox/LoxClass.cs
@@ -1,12 +1,20 @@
-﻿namespace cslox;
+﻿
+namespace cslox;
 
-internal class LoxClass
+internal class LoxClass : ILoxCallable
 {
     internal readonly string Name;
 
     internal LoxClass(string name)
     {
         Name = name;
+    }
+
+    public int Arity() => 0;
+
+    public object? Call(Interpreter interpreter, IEnumerable<object?> arguments)
+    {
+        return new LoxInstance(this);
     }
 
     public override string ToString() => Name;
---
+++ cslox/LoxInstance.cs
@@ -0,0 +1,13 @@
+﻿namespace cslox;
+
+internal class LoxInstance
+{
+    private readonly LoxClass m_class;
+
+    internal LoxInstance(LoxClass @class)
+    {
+        m_class = @class;
+    }
+
+    public override string ToString() => $"{m_class.Name} instance";
+}
